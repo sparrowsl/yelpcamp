7aff3b2d437840edf2dd4b618818a1ef84c8fa5e
sparrowsl <benthorpe.dev@gmail.com>
2023-09-29 12:50:29 +0000

    feat: style campgrounds

--- frontend/src/routes/Navbar.svelte
+++ frontend/src/routes/Navbar.svelte
@@ -1,12 +1,11 @@
 <div class="bg-gray-700 text-white">
-	<nav class="container flex items-center justify-between font-roboto py-3">
+	<nav class="container flex items-center gap-10 font-roboto py-3">
 		<a href="/" class="block text-2xl font-bold">YelpCamp</a>
 
 		<ul class="flex items-center gap-5 text-sm">
 			<li><a href="/">Home</a></li>
 			<li><a href="/campgrounds">Campgrounds</a></li>
 			<li><a href="/campgrounds/new">New Campground</a></li>
-			<li><a href="/">Home</a></li>
 		</ul>
 	</nav>
 </div>
--- frontend/src/routes/campgrounds/+page.svelte
+++ frontend/src/routes/campgrounds/+page.svelte
@@ -6,10 +6,29 @@
 <h1 class="text-3xl font-bold">All Campgrounds...</h1>
 <a href="/campgrounds/new" class="text-blue underline mb-2 block">Add Campground</a>
 
-<ul class="list-disc list-inside pl-4">
-	{#each data.campgrounds as campground}
-		<li>
-			<a class="underline text-blue-500" href="/campgrounds/{campground.id}">{campground.title}</a>
-		</li>
+<section class="list-disc list-inside pl-4">
+	{#each data.campgrounds as campground (campground.id)}
+		<figure class="flex gap-5 border">
+			<img
+				src={campground.image}
+				alt={campground.title}
+				loading="lazy"
+				class="w-70 h-45 object-contain"
+			/>
+
+			<figcaption>
+				<h3 class="font-bold text-lg">{campground.title}</h3>
+				<p class="text-gray-900">{campground.description}</p>
+
+				<p class="text-sm text-gray-600 my-2">{campground.location}</p>
+
+				<a
+					class="block text-white bg-blue-600 text-sm w-fit px-4 py-1 rounded"
+					href="/campgrounds/{campground.id}"
+				>
+					View {campground.title}
+				</a>
+			</figcaption>
+		</figure>
 	{/each}
-</ul>
+</section>
