f59fe9a292324ddb2aa37320de58e750fe53362a
sparrowsl <benthorpe.dev@gmail.com>
2023-09-29 08:29:32 +0000

    redesign update route

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+	/** @type {"submit"|"reset"|"button"} */
+	export let type = "submit";
+</script>
+
+<button
+	{type}
+	class="{$$props.class} block bg-gray-800 text-white rounded-sm text-sm py-2 px-6"
+	on:click
+>
+	<slot>Click Me</slot>
+</button>
---
+++ frontend/src/lib/components/FormInput.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let type = "text";
+	export let name = "";
+	export let id = "";
+	export let placeholder = "";
+	export let label = "";
+	export let required = true;
+	export let value = "";
+</script>
+
+<label for={id || name} class="block">
+	{#if label.trim()}<span>{label}</span>{/if}
+	<input
+		{type}
+		{name}
+		id={id || name}
+		{placeholder}
+		{required}
+		{value}
+		on:input={(/** @type {any} */ e) => (value = e.target.value)}
+		class="{$$props.class} block rounded text-sm"
+	/>
+</label>
